abe4177cfc39681f3e2e798e3dd87cdec6fae841
ajay <ajaykrdtg5@gmail.com>
2024-05-16 01:08:17 +0530

    chnages in files

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const cors = require('cors');
-const axios = require('axios');
 const os = require('os');
 
 const app = express();
-
 app.use(cors());
 
 // Function to get the local IP address
@@ -32,23 +30,13 @@ const getSystemDetails = () => {
     };
 };
 
-app.get('/api/ip', async (req, res) => {
-    try {
-        const publicIpResponse = await axios.get('https://api.ipify.org?format=json');
-        const publicIp = publicIpResponse.data.ip;
-        const ipApiResponse = await axios.get(`http://ip-api.com/json/${publicIp}`);
-        const publicIpDetails = ipApiResponse.data;
-        const localIp = getLocalIpAddress();
-        const systemDetails = getSystemDetails();
-        res.json({ publicIp, publicIpDetails, localIp, systemDetails });
-    } catch (error) {
-        console.error('Error fetching IP address:', error);
-        res.status(500).send('Error fetching IP address');
-    }
+app.get('/api/ip', (req, res) => {
+    const localIp = getLocalIpAddress();
+    const systemDetails = getSystemDetails();
+    res.json({ localIp, systemDetails });
 });
 
-// Use a specific port or let the OS choose an available port
-const PORT = process.env.PORT || 5001;
-const server = app.listen(PORT, () => {
-    console.log(`Server running on port ${server.address().port}`);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
